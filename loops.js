e491c55a70d6772303bf71862a4273063d9a016a
George Boots <ggboots.99@gmail.com>
2020-06-26 09:49:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,11 +1,12 @@
 function forLoop(array){
-  for(let i= 0; i < 25; i++){
+  for(let i= 0; i < 25; i++) {
     if(i === 1){
       array.push('I Am 1 strange loop.');
     } else {
       array.push(`I am ${i} strange loop.`)
     }
   }
+  
   return array;
 }
 
